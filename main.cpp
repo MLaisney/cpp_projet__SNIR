69fc3ea28d4cef54f64a10d1cb9e459298e6a790
MLaisney <laisney.melvyn@lyceejulesfil.fr>
2023-09-25 16:33:05 +0200

    Enter name

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*Nom : Questionnaire des SNIR
+Par : Laisney Melvyn, le 25/09/2023*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string nom; //Entrée de l'indentité 
+    cout << "Entrez votre nom : ";
+    getline (cin, nom);
+    cout << nom << endl;
+
+    return 0;
+}
