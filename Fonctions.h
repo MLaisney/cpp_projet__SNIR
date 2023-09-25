be82b2f8890927d0f2a9caccb61a48f907fd15cb
MLaisney <laisney.melvyn@lyceejulesfil.fr>
2023-09-25 16:47:47 +0200

    Quiz fonctions

--- Fonctions.h
+++ Fonctions.h
@@ -3,8 +3,17 @@
 
 #include <string>
 
-struct Questionnaire {
+struct Questionnaire 
+{
     std::string theme;
     int bonnesReponses;
     int mauvaisesReponses;
 };
+
+//Fonctions des questionnaires
+void creerQuestionnaire(std::string theme, std::string nomFichier);
+void lireQuestionnaire(std::string nomFichier, Questionnaire& questionnaire);
+void analyserQuestionnaires(Questionnaire questionnaires[], int nbQuestionnaires, std::string nom);
+void genererRapport(std::string nom, Questionnaire questionnaires[], int nbQuestionnaires);
+
+#endif // FONCTIONS_H
