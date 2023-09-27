c7310bcb78e85e56b4018e542b4c285234e33807
MLaisney <laisney.melvyn@lyceejulesfil.fr>
2023-09-27 16:40:13 +0200

    add comments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 /*Nom : Questionnaire des SNIR
 Par : Laisney Melvyn, le 25/09/2023*/
 
+//appel des librairies
 #include "Fonctions.h"
 #include <fstream>
 #include <iostream>
@@ -8,22 +9,24 @@ Par : Laisney Melvyn, le 25/09/2023*/
 
 using namespace std;
 
+// Programme principal
 int main()
 {
     string nom;
-    demanderIdentite(nom);
+    demanderIdentite(nom); //appel de la fonction pour l'identite
 
+//initialisation à 0 des variables
     int bonnesReponses = 0;
     int mauvaisesReponses = 0;
-    genererQuestionnaire(bonnesReponses, mauvaisesReponses);
+    genererQuestionnaire(bonnesReponses, mauvaisesReponses); //appel de la fonction du questionnaire
 
     ofstream fichier("resultats.txt");
-    if (fichier.is_open()) {
-        fichier << "Nom : " << nom << endl;
+    if (fichier.is_open()) { //ouverture du fichier 
+        fichier << "Nom : " << nom << endl; //écriture du fichier 
         fichier << "Bonnes réponses : " << bonnesReponses << endl;
         fichier << "Mauvaises réponses : " << mauvaisesReponses << endl;
-        fichier.close();
-    } else {
+        fichier.close(); //fermeture du fichier
+    } else { //affichage du message d'erreur d'ouverture
         cout << "Impossible d'ouvrir le fichier pour enregistrer les résultats." << endl;
     }
 
