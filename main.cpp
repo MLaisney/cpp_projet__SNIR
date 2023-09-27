9a7c81f0bb01cd228e1d958bde2019276caa3060
MLaisney <laisney.melvyn@lyceejulesfil.fr>
2023-09-27 13:31:35 +0200

    Modife all main

--- main.cpp
+++ main.cpp
@@ -2,31 +2,32 @@
 Par : Laisney Melvyn, le 25/09/2023*/
 
 #include <iostream>
+#include "Fonctions.h"
 using namespace std;
 
 int main()
 {
+    //Initialisation du nombre de bonnes et mauvaises réponse
+    int bonnesReponses = 0;
+    int mauvaisesReponses = 0;
+    
     //Entrée de l'indentité 
     string nom;
     cout << "Entrez votre nom : ";
     getline (cin, nom);
     cout << nom << endl;
 
-    // Création des questionnaires
-    creerQuestionnaire("Mathematiques", "mathematiques.txt");
-    creerQuestionnaire("Histoire", "histoire.txt");
-    creerQuestionnaire("Geographie", "geographie.txt");
-    creerQuestionnaire("Science", "science.txt");
-
-    // Lire les questionnaires
-    Questionnaire questionnaires[4];
-    lireQuestionnaire("mathematiques.txt", questionnaires[0]);
-    lireQuestionnaire("histoire.txt", questionnaires[1]);
-    lireQuestionnaire("geographie.txt", questionnaires[2]);
-    lireQuestionnaire("science.txt", questionnaires[3]);
-
-    // Analyser les questionnaires
-    analyserQuestionnaires(questionnaires, 4, nom);
+    genererQuestionnaire(bonnesReponses, mauvaisesReponses);
+    //Affichage des résultats
+    ofstream fichier("resultats.txt");
+    if (fichier.is_open()) {
+        fichier << "Nom : " << nom << endl;
+        fichier << "Bonnes réponses : " << bonnesReponses << endl;
+        fichier << "Mauvaises réponses : " << mauvaisesReponses << endl;
+        fichier.close();
+    } else {
+        cerr << "Impossible d'ouvrir le fichier pour enregistrer les résultats." << endl; //Message d'erreur
+    }
 
     return 0;
 }
