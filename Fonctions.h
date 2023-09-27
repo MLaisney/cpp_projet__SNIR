79f04d170adf6b91924b373fde3f3f60ddc03104
MLaisney <laisney.melvyn@lyceejulesfil.fr>
2023-09-27 16:41:25 +0200

    add comments

--- Fonctions.h
+++ Fonctions.h
@@ -3,13 +3,13 @@ Par : Laisney Melvyn, le 25/09/2023*/
 
 #ifndef FONCTIONS_H
 #define FONCTIONS_H
-
+//appel des librairies
 #include <string>
 #include <iostream>
 #include <fstream>
 
 using namespace std;
-
+//repertoire des fonctions
 void demanderIdentite(string& nom);
 void genererQuestionnaire(int& bonnesReponses, int& mauvaisesReponses);
 
