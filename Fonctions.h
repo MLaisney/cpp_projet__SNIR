c487fcc5e8d531beade95a78e7955439d5b659d3
MLaisney <laisney.melvyn@lyceejulesfil.fr>
2023-09-28 10:30:19 +0200

    test

--- Fonctions.h
+++ Fonctions.h
@@ -5,11 +5,12 @@ Par : Laisney Melvyn, le 25/09/2023*/
 #define FONCTIONS_H
 //appel des librairies
 #include <string>
-#include "Fonctions.cpp"
 
 using namespace std;
 //repertoire des fonctions
 void demanderIdentite(string& nom);
 void genererQuestionnaire(int& bonnesReponses, int& mauvaisesReponses);
+#include "Fonctions.cpp"
+
 
 #endif // FONCTIONS_H
